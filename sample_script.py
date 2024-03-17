b45f988a258f1b8cca85e1d6ec79e32bc2c6539d
Ilya Dyakov <dyak.ilya@gmail.com>
2024-03-16 22:17:18 -0700

    Update automated search script

--- sample_script.py
+++ sample_script.py
@@ -14,7 +14,7 @@ driver.maximize_window()
 
 # open the url
 driver.get('https://www.google.com/')
-
+#driver.get('https://www.target.com/')
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
