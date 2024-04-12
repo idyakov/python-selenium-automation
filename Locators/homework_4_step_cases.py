d3ea73c1c24b12562dacb09f95694d1a67e7a97a
Ilya Dyakov <dyak.ilya@gmail.com>
2024-04-11 17:50:50 -0700

    hw_4

--- Locators/homework_4_step_cases.py
+++ Locators/homework_4_step_cases.py
@@ -12,64 +12,51 @@ service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
-# open the url
 driver.get('https://www.target.com/l/target-circle/-/N-pzno9')
-sleep(3)
-# #step cases
-# #Find the logo
-
-
 
 expected = 6
-deals = driver.find_elements(By.XPATH, '//div[@data-component-id="WEB-397697"] //a[@data-test="@web/slingshot-components/CellsComponent/Link"]')
+deals = driver.find_elements(By.XPATH,'//div[@data-component-id="WEB-397697"] //a[@data-test="@web/slingshot-components/CellsComponent/Link"]')
 numbers = int(len(deals))
-print(numbers)
 assert expected == numbers
-print(f'Expected deals {expected}, got {numbers}')
+print(f'Expected deals {expected}, got {numbers}. Gongrats! Go and take your cookies from the table.')
 sleep(3)
 
 
 
+driver.get('https://www.target.com/')
 
+driver.find_element(By.ID, 'search').send_keys('russian hat')  # item search
+sleep(2)
+driver.find_element(By.XPATH, "//button[@type='submit']").click()  # prees the loop
+sleep(5)
+driver.find_element(By.XPATH, "//button[@id='addToCartButtonOrTextIdFor88282075']").click()  # press the button add to cart
+sleep(8)
+driver.find_element(By.XPATH, "//div[@class='ReactModal__Content ReactModal__Content--after-open styles__StyledReactModal-sc-10966vp-0 cfemXU']//button[text()='Add to cart']").click()
+sleep(8)
+driver.find_element(By.CSS_SELECTOR, "a[href='/cart']").click()
+sleep(5)
 
+item = driver.find_element(By.CSS_SELECTOR, "div[data-test='cartItem-title']").text
+print(f'Here is your item - {item}. Go ahead, American boy, wear it and meet a winter time.')
+sleep(3)
+text1 = driver.find_element(By.CSS_SELECTOR, "div[class='styles__StyledHeading-sc-1ge2jts-1 bmsjWz']").text
+print(f'The price is {text1} including the taxes and the shipping')
+sleep(7)
 
+driver.get('https://help.target.com/help')
 
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# driver.find_element(By.XPATH, "//i[@class='a-icon a-icon-logo']")
-# print("Test case - passed. Logo has been found")
-# #Find email field # selector #ap_email
-# driver.find_element(By.XPATH, "//input[@type='email']")
-# print("Test case by the type - email - passed. Email field has been found")
-# driver.find_element(By.XPATH, "//input[@id='ap_email']")
-# print("Test case by ID - ap_email - passed. Email field has been found")
-# #Find Cintinue button #
-# driver.find_element(By.XPATH, "//input[@id='continue']")
-# print("Test case Continue button - by id - passed.")
-# driver.find_element(By.XPATH, "//input[@class='a-button-input']")
-# print("Test case Continue button - by class - passed.")
-# driver.find_element(By.XPATH, "//*[contains(@class, 'a-button-input')]")
-# print("Test case Continue button - by contains - passed.")
-# driver.find_element(By.XPATH, "//a[text()='Privacy Notice']")
-# print("Test case Privacy Notice - by Text - passed.")
-# driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']")
-# print("Test case <Need help?> - by tag span and the attribute class - passed.")
-# driver.find_element(By.XPATH, "//a[@id='auth-fpp-link-bottom']")
-# print("Test case <Forgot your password?> - by tag id - passed.")
-# driver.find_element(By.XPATH, "//a[@id='ap-other-signin-issues-link' and contains(@href, 'account-issues')]")
-# print("Test case <Other issues with Sign-In> - by id - passed.")
-# driver.find_element(By.ID, 'createAccountSubmit')
-# print("Test case <Create your Amazon account> - by id - passed.")
-# driver.find_element(By.XPATH,"//a[@id='createAccountSubmit' and contains(@href, 'ap')]")
-# print("Test case <Create your Amazon account> - by id and contains (portion) - passed.")
+driver.find_element(By.CSS_SELECTOR, "h2[class='custom-h2']")
+print("Target help header - passed")
+driver.find_element(By.ID, 'j_id0:j_id2:j_id32:name')
+print("Target search panel - passed")
+driver.find_element(By.CSS_SELECTOR, "input[class='btn-sm search-btn']")
+print("Target search loop icon - passed")
+driver.find_element(By.CSS_SELECTOR, "div[class='container clearfix']")
+print("Target action steps to do - passed")
+driver.find_element(By.CSS_SELECTOR, "div[class='salesforceBox txtAC bigbox1']")
+print("Target Manage my lines - passed")
+driver.find_element(By.CSS_SELECTOR, "div[class='grid_4 boxSmallr txtAC bigbox2']")
+print("Target Contact us - passed")
+driver.find_element(By.XPATH, "//h2[text()='Browse all Help pages']")
+print("Target Browse all help pages - passed")
+sleep(5)
